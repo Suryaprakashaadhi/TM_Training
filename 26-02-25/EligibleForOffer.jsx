6c1b2e6608227d339189940a4bc3972a08e5137b
Suryaprakash M <146814830+Suryaprakashaadhi@users.noreply.github.com>
2025-02-26 12:53:02 +0530

    Update EligibleForOffer.jsx

--- 26-02-25/EligibleForOffer.jsx
+++ 26-02-25/EligibleForOffer.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function CheckGovtOffer(props) {
+  const { hasAadhar, hasPAN } = props;
+
+  if (hasAadhar || hasPAN) {
+    return (
+      <h2>
+        User is eligible for the government offer! Cards Available:{" "}
+        {hasAadhar ? "Aadhar " : ""} {hasPAN ? "PAN" : ""}
+      </h2>
+    );
+  } else {
+    return <h2> User is NOT eligible for the government offer.</h2>;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CheckGovtOffer hasAadhar={false} hasPAN={true} />);
 
